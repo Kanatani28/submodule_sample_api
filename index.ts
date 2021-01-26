6736a041fb77ffb2e9545fd6461a06c42dd31195
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2021-01-26 15:19:10 +0900

    initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+
+const router: express.Router = express.Router()
+router.get('/', (req:express.Request, res:express.Response) => {
+  res.send({message: "Hello World"})
+})
+app.use(router)
+
+app.listen(3000, ()=>{ console.log('Example app listening on port 3000!') })
